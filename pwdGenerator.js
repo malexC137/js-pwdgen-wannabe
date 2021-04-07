21c4bc6a88a9cbdb0e44aa82addeadc7940ac236
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-04-07 17:08:51 +0200

    End project

--- pwdGenerator.js
+++ pwdGenerator.js
@@ -1,7 +1,11 @@
 var nameUser = prompt("Qual'è il tuo nome?");
 var surname = prompt("Qual'è il tuo cognome?");
-var favColor = prompt("Qual'è il tuo colore preferito?")
+var favColor = prompt("Qual'è il tuo colore preferito?");
+var age = prompt("Qual'è la tua età?");
 var pwdGenerated = nameUser + surname + favColor;
 var todayDate = new Date().getFullYear();
+var birthDate = todayDate - age;
 
-console.log(pwdGenerated + todayDate);
+document.getElementById("generatedPassword1").innerHTML = pwdGenerated + "21";
+document.getElementById("generatedPassword2").innerHTML = pwdGenerated + todayDate;
+document.getElementById("generatedPassword3").innerHTML = pwdGenerated + birthDate;
